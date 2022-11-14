ab76aaea3718b730483b7ebf4ced246d5d12c3a6
ljbee22 <63335985+ljbee22@users.noreply.github.com>
2022-11-14 22:55:03 +0900

    main.dart 업데이트

--- map_test/lib/main.dart
+++ map_test/lib/main.dart
@@ -2,7 +2,6 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:location/location.dart';
 import 'package:naver_map_plugin/naver_map_plugin.dart';
-import 'package:geolocator/geolocator.dart';
 
 void main() {
   runApp(MyApp());
@@ -27,50 +26,14 @@ class NaverMapTest extends StatefulWidget {
 class _NaverMapTestState extends State<NaverMapTest> {
   Completer<NaverMapController> _controller = Completer();
   List<Marker> markers = [];
-  late LocationData? currentLocation;
   final MapType _mapType = MapType.Basic;
+  bool buttonBool = false;
 
   CameraPosition initialPosition = const CameraPosition(
       target: LatLng(37.4592,126.9521), zoom: 16
   );
 
-  void updateCurrentLocation() async {
-    Location tmplocation = Location();
-
-    tmplocation.getLocation().then(
-          (location) {
-        currentLocation = location;
-      },
-    );
-
-    NaverMapController naverMapController = await _controller.future;
-
-    tmplocation.onLocationChanged.listen(
-          (newLoc) {
-        currentLocation = newLoc;
-
-        naverMapController.moveCamera(
-          CameraUpdate.toCameraPosition(
-            CameraPosition(
-              zoom: 13.5,
-              target: LatLng(
-                newLoc.latitude!,
-                newLoc.longitude!,
-              ),
-            ),
-          ),
-        );
-        setState(() {});
-      },
-    );
-  }
-
-
   @override
-  initState(){
-    super.initState();
-    updateCurrentLocation();
-  }
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('NaverMap Test')),
@@ -82,18 +45,43 @@ class _NaverMapTestState extends State<NaverMapTest> {
       ),
       floatingActionButton: FloatingActionButton.extended(
           onPressed: () async{
-            Position myPosition = await _myposition();
+            buttonBool = !buttonBool;
+            late LocationData currentLocation;
+            Location tmplocation = Location();
 
-            NaverMapController controller = await _controller.future;
-            controller.moveCamera(
-                CameraUpdate.toCameraPosition(
-                    CameraPosition(
-                        target: LatLng(myPosition.latitude, myPosition.longitude),
-                        zoom: 16
-                    )
-                )
+            print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+            print(buttonBool);
+            print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+
+            tmplocation.getLocation().then(
+                  (location) {
+                currentLocation = location;
+              },
             );
-            setState(() {});
+
+            if (buttonBool){
+              tmplocation.onLocationChanged.listen(
+                (newLoc) async {
+                  currentLocation = newLoc;
+
+                  NaverMapController naverMapController =
+                      await _controller.future;
+
+                  naverMapController.moveCamera(
+                    CameraUpdate.toCameraPosition(
+                      CameraPosition(
+                        zoom: 16,
+                        target: LatLng(
+                          newLoc.latitude!,
+                          newLoc.longitude!,
+                        ),
+                      ),
+                    ),
+                  );
+                  setState(() {});
+                },
+              );
+            }
           },
           label: const Icon(Icons.location_on_outlined)
       ),
@@ -104,33 +92,4 @@ class _NaverMapTestState extends State<NaverMapTest> {
     if (_controller.isCompleted) _controller = Completer();
     _controller.complete(controller);
   }
-}
-
-Future<Position> _myposition() async{
-  bool serviceEnabled;
-  LocationPermission permission;
-
-  serviceEnabled = await Geolocator.isLocationServiceEnabled();
-
-  if(!serviceEnabled) {
-    return Future.error("Location services are disabled");
-  }
-
-  permission = await Geolocator.checkPermission();
-
-  if(permission  == LocationPermission.denied) {
-    permission = await Geolocator.requestPermission();
-
-    if(permission == LocationPermission.denied) {
-      return Future.error("Location Permission denied");
-    }
-  }
-
-  if(permission == LocationPermission.deniedForever) {
-    return Future.error("Location Permission permanently denied");
-  }
-
-  Position position = await Geolocator.getCurrentPosition();
-
-  return position;
 }
